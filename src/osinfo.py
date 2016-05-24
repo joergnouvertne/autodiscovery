42bfee207fd2ff0a0e11968b6425045020f06251
joergnouvertne <mail@joergnouvertne.de>
2016-05-24 10:03:10 +0200

    Added get_os_name function

--- src/osinfo.py
+++ src/osinfo.py
@@ -3,5 +3,12 @@ import platform
 
 class OSInfo:
     def __init__(self):
-        self.platform_name = platform.system()
+        self.os_name = self.get_os_name()
 
+    def get_os_name(self):
+        os_name = platform.system()
+        if os_name == 'Darwin':
+            os_name = 'MacOS'
+        elif not os_name:
+            os_name = 'Unknown'
+        return os_name
---
+++ test_src/test_osinfo.py
@@ -0,0 +1,5 @@
+from src.osinfo import OSInfo
+
+server = OSInfo()
+
+print server.os_name
